a4031fbc55862681de98d1368aa3b601ce8f4faa
Malleswari <malleswari.gelli@syngenta.com>
2025-07-20 16:36:14 -0500

    setup added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name='document_portal',
+    author="Malleswari Gelli",
+    version='0.1',
+    packages=find_packages(),
+    description='A document portal for querying documents using LLMs',
+)
