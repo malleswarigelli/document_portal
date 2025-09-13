b41516a64684e4bd5260c2a0a83b15d67091cfb5
Malleswari <malleswari.gelli@syngenta.com>
2025-09-13 10:49:13 -0500

    Document compare code updated

--- src/document_compare/document_compare.py
+++ src/document_compare/document_compare.py
@@ -12,12 +12,36 @@ from langchain.output_parsers import OutputFixingParser
 
 class DocumentComparatorLLM():
     def __init__(self, logger: CustomLogger):
-        pass
+        load_dotenv() # load environment variables
+        self.log = logger
+        self.model_loader = ModelLoader()
+        self.embedding_model = self.model_loader.load_embeddings()
+        self.llm_model = self.model_loader.load_llm()
+        self.json_parser = JsonOutputParser(pydantic_object=SummaryResponse)
+        self.output_parser = OutputFixingParser.from_llm(
+            llm=self.llm_model,
+            parser=self.json_parser,
+            max_tries=3,
+            verbose=True
+        )
+        self.prompt = PROMPT_REGISTRY["document_compare_prompt"]
+        self.chain = self.prompt | self.llm_model | self.json_parser | self.output_parser
+        self.log.info("DocumentComparatorLLM initialized with model and parser successfully")
 
     def compare_documents(self):
         '''Compares two documents and returns the differences.'''
-        pass
+        try:
+            pass
+
+        except Exception as e:
+            self.log.error(f"Error comparing documents, {e}")
+            raise DocumentPortalException("Error occurred while comparing documents", sys)
 
     def _format_response(self):
         '''Formats the LLM response into a structured format.'''
-        pass
+        try:
+            pass
+
+        except Exception as e:
+            self.log.error("Error formatting response into DataFrame", error=str(e))
+            raise DocumentPortalException("Error formatting response", sys)
